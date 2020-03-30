53bea47ca6ae004693bc38a4797f4008e8458bd8
José Meléndez <josemel1103@gmail.com>
2020-03-29 18:02:06 -0600

    Se corrigieron errores en la carga de archivo

--- Fase2LFA/Fase2LFA/Form1.cs
+++ Fase2LFA/Fase2LFA/Form1.cs
@@ -14,6 +14,40 @@ namespace Fase2LFA
 {
     public partial class Form1 : Form
     {
+        //Arreglo que contiene todas las líneas del archivo
+        ArrayList Lineas = new ArrayList();
+        //Método de Lectura
+        void Lectura()
+        {
+            string Ruta = "";
+            using (OpenFileDialog Selección = new OpenFileDialog())
+            {
+                Selección.Filter = "txt files (*.txt)|*.txt";
+                if (Selección.ShowDialog() == DialogResult.OK)
+                {
+                    //Obtener la dirección de archivo seleccionado
+                    Ruta = Selección.FileName;
+                    tbDirección.Text = Ruta;
+                    var LeerArchivo = Selección.OpenFile();
+                    using (StreamReader Lector = new StreamReader(LeerArchivo))
+                    {
+                        string LineaLeida = "";
+                        while ((LineaLeida = Lector.ReadLine()) != null)
+                        {
+                            if (LineaLeida != "")
+                            {
+                                Lineas.Add(LineaLeida);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Para continuar debe seleccionar algún archivo \".txt\", por favor intente de nuevo");
+                    Lectura();
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +55,8 @@ namespace Fase2LFA
 
         private void btCargar_MouseClick(object sender, MouseEventArgs e)
         {
-            
+            lbProceso.Visible = true;
+            Lectura();
         }
     }
 }
